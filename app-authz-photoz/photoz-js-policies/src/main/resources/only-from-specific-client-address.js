957ad1bb3d63836cdcc70eda27848f62cf83d622
ackerleytng <ackerleytng@gmail.com>
2020-04-22 18:55:15 +0800

    Add print allow better understanding of authorization going on at keycloak

--- app-authz-photoz/photoz-js-policies/src/main/resources/only-from-specific-client-address.js
+++ app-authz-photoz/photoz-js-policies/src/main/resources/only-from-specific-client-address.js
@@ -1,5 +1,6 @@
 var contextAttributes = $evaluation.getContext().getAttributes();
 
 if (contextAttributes.containsValue('kc.client.network.ip_address', '127.0.0.1')) {
+    print(contextAttributes.getValue('kc.client.network.ip_address').asString(0));
     $evaluation.grant();
 }
--- app-authz-photoz/photoz-js-policies/src/main/resources/only-owner.js
+++ app-authz-photoz/photoz-js-policies/src/main/resources/only-owner.js
@@ -3,6 +3,9 @@ var identity = context.identity;
 var permission = $evaluation.permission;
 var resource = permission.resource;
 
+print(resource.owner);
+print(identity.id);
+
 if (resource.owner == identity.id) {
     $evaluation.grant();
 }
